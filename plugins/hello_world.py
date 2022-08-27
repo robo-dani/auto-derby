0ba07267aa43499230eccba97960664e2e1071d3
Zhifan Huang <zf.huang6@outlook.com>
2022-08-27 17:59:55 +0900

    chore: remove useless plugins

--- plugins/hello_world.py
+++
@@ -1,9 +0,0 @@
-import auto_derby
-
-
-class Plugin(auto_derby.Plugin):
-    def install(self) -> None:
-        print("hello world")
-
-
-auto_derby.plugin.register(__name__, Plugin())
--- plugins/use_legacy_screenshot.py
+++
@@ -1,9 +0,0 @@
-import auto_derby
-
-
-class Plugin(auto_derby.Plugin):
-    def install(self) -> None:
-        auto_derby.config.use_legacy_screenshot = True
-
-
-auto_derby.plugin.register(__name__, Plugin())
